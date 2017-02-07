a30ca9b7035ce437715a76e1783cd8adbc106f8c
Brittany Walker <breneewalker@gmail.com>
2017-02-06 23:31:11 -0500

    add images, update CSS, add content

---
+++ app.js
@@ -0,0 +1,15 @@
+/* Anchor scrolling code from W3 Schools */
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
